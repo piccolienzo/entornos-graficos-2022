9ed8605badfb8f7b0d52498bd6c40490a32780d5
alejo-cuello <alejo@diproach.com>
2022-06-18 12:24:16 -0300

    backend: script listado de usuarios ok

--- HorariosConsulta/controllers/users/users.php
+++ HorariosConsulta/controllers/users/users.php
@@ -1,24 +1,17 @@
 <?php
-    //Agreguen su archivo connection.inc con sus credenciales para entrar a MySQl
-    //Les dejo uno de ejemplo
-    include('./connection.inc');
-    $query = "select * from usuarios where email like ".$email;
+    extract($_GET);
+
+    include("../connection.inc");
+    $query = "select * from usuarios";
+    if( isset($textSearch) ) $query .= " where legajo like '%".$textSearch."%' or nombre like '%".$textSearch."%' or apellido like '%".$textSearch."%'";
     $result = mysqli_query($link, $query) or die(mysqli_error($link));
+    
+    echo("<table>");
 
-    if(mysqli_num_rows($result) == 0) {
-        echo("Usuario no válido");
-    }
-    else {
-        $fila = mysqli_fetch_array($vResultado);
-        if($fila["clave"] == md5($password)) {
-            session_start();
-            echo(fila["legajo"]);
-            $_SESSION["usuario"] = $fila;
-        }
-        else {
-            echo("Contraseña incorrecta");
-        }
+    while($row = mysqli_fetch_array($result)) {
+        //TO DO: DAR EL FORMATO NECESARIO PARA LA PAGINA
+        echo("<tr>".$row["nombre"]."</tr>");
     }
 
-    mysqli_close($link);
+    echo("</table>");
 ?>
